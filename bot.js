14620b9cde8443098c9a3e4fe7cc3784f1bd4cef
Glitch (sunucubotu) <none>
2018-11-29 14:17:34 +0000

    🌏🚋 Checkpoint ./bot.js:2026984/1669 ./komutlar/level.js:2026984/2733

--- bot.js
+++ bot.js
@@ -149,56 +149,6 @@ client.on('guildMemberAdd', async member => {
 
 ////////////////////////
 
-let points = JSON.parse(fs.readFileSync('./xp.json', 'utf8'));
-
-var f = [];
-function factorial (n) {
-  if (n == 0 || n == 1)
-    return 1;
-  if (f[n] > 0)
-    return f[n];
-  return f[n] = factorial(n-1) * n;
-};
-function clean(text) {
-  if (typeof(text) === "string")
-    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-  else
-      return text;
-}
-
-client.on("message", async message => {
-    if (message.channel.type === "dm") return;
-
-  if (message.author.bot) return;
-
-  var user = message.mentions.users.first() || message.author;
-  if (!message.guild) user = message.author;
-
-  if (!points[user.id]) points[user.id] = {
-    points: 0,
-    level: 0,
-  };
-
-  let userData = points[user.id];
-  userData.points++;
-
-  let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));
-  if (curLevel > userData.level) {
-    userData.level = curLevel;
-        var user = message.mentions.users.first() || message.author;
-message.channel.send(`🆙 **| ${user.username}   Level Atladı!*`)
-    }
-
-fs.writeFile('./xp.json', JSON.stringify(points), (err) => {
-    if (err) console.error(err)
-  })
-
-  if (message.content.toLowerCase() === prefix + 'profil' || message.content.toLowerCase() === prefix + 'profile') {
-const level = new Discord.RichEmbed().setTitle(`${user.username}`).setDescription(`**Seviye:** ${userData.level}\n**Xp:** ${userData.points}`).setColor("#ffff00").setFooter(``).setThumbnail(user.avatarURL)
-message.channel.send(`📝 **| ${user.username} adlı kullanıcının profil kartı**`)
-message.channel.send(level)
-  }
-});
 
 ////////////////////////
 
