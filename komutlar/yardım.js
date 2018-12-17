baf7d4559b48ae277d6783c001379a64288f2f7d
Glitch (sunucubotu) <none>
2018-12-17 15:09:50 +0000

    🌧🎐 Checkpoint ./komutlar/yardım.js:2026984/946

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -4,20 +4,6 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 exports.run = (client, message, params) => {
-  const embed = new Discord.RichEmbed()
-  .setTitle("**KOMUTLAR**")
-  .setColor(0x2bff00)
-  .addField("》rb!kaydol","Sunucuya kaydolmanızı sağlar.")
-  .addField("》rb!js","JS kanallarına erişim sağlar.")
-  .addField("》rb!canlıdestek",".")
-  .addField("》rb!hatalı-komut <#kanal>","")
-  .addField("》rb!istatistik","")
-  .addField("》rb!istek-kod","İ")
-  .addField("》rb!kullanıcıbilgim","K")
-  .addField("》rb!öneri <öneriniz> ","Bot sahiplerine öneride bulunmanızı sağlar.")
-  .addField("》rb!ping","Botun pingini görmenizi sağlar.")
-  .addField("Diğer","[**Davet**](https://discordapp.com/oauth2/authorize?client_id=513097500748546049&scope=bot&permissions=8) | [**Sunucu**](https://discord.gg/dDYsvyy)")
-  .setFooter(`${message.author.tag} tarafından istendi.`, message.author.avatarURL)
   
   if ([0]) {
     message.channel.sendCode("asciidoc", `= Real Bot Komutlar =
@@ -28,9 +14,9 @@ rb!canlıdestek     :: Yetkili kişilerle canlı desteğe geçmenizi sağlar.
 rb!hatalıkomut     :: Belirttiğiniz komutun hatalı olduğunu yetkili kişilere bildirir.
 rb!istatistik      :: Botun istatistiklerini görmenizi sağlar.
 rb!istek-kod       :: İstediğiniz kodu Paylaşımcılara bildirir.
-rb!kullanıcıbilgi  :: 
-rb!öneri           :: 
-rb!ping            :: 
+rb!kullanıcıbilgi  :: Kullanıcı bilgilerinizi görmenizi sağlar.
+rb!öneri           :: Bot sahiplerine öneride bulunmanızı sağlar.
+rb!ping            :: Botun pingini görmenizi sağlar.
 
 = Müzik Komutları =
 
