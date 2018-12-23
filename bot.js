f6c023f177a85f5ed625be58e2c0c955644d612f
Glitch (sunucubotu) <none>
2018-12-23 18:28:06 +0000

    🍬🌲 Checkpoint ./komutlar/giriş-çıkış.js:2045523/5255 ./events/guildMemberAdd.js:2045523/204 ./bot.js:2045523/16828 ./events/guildMemberRemove.js:2045523/204

--- bot.js
+++ bot.js
@@ -267,4 +267,4 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(ayarlar.token);
+client.login(ayarlar.token);client.login(ayarlar.token);
