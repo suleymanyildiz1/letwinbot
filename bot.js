f945d3583273019ccaa4f7a001b2ad4f57c7525b
Glitch (sunucubotu) <none>
2019-01-05 13:30:06 +0000

    😀👸 Checkpoint ./bot.js:2045523/311

--- bot.js
+++ bot.js
@@ -114,7 +114,15 @@ client.on("guildMemberRemove", async member => {
 
 ////////////////////////
 
-
+    client.on('message', async msg => {
+    if (msg.content.toLowerCase() === prefix + "disko") {
+   if (msg.channel.type === "dm") return;
+  const rol = '+' // Rol ismi buraya
+  setInterval(() => {
+      msg.guild.roles.find(s => s.name === rol).setColor("RANDOM")
+      }, 8000);
+  }
+});
 
 ////////////////////////
  
