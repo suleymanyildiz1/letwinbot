0e17b4f14e58bc6faeca6aeec2f6b6c24fd46181
Glitch (sunucubotu) <none>
2019-01-18 14:36:09 +0000

    💍🍱 Checkpoint ./komutlar/disco-durdur.js:2045523/818

--- komutlar/disco.js
+++ komutlar/disco.js
@@ -3,28 +3,26 @@ const db = require('quick.db');
 
 exports.run = async (client, msg, args) => {
   
-  let hayir = (client.emojis.find("name", "CARRno").toString())
-  let evet = (client.emojis.find("name", "CARRyes").toString())
 
   if (!msg.member.hasPermission ("ADMINISTRATOR")) return msg.reply(`Bu Komutu Kullanmak İçin **Yönetici** İznine Sahip Olmalısın!`);
-
+  
   let discoR = await db.fetch(`discorol_${msg.guild.id}`);
    setInterval(() => {
-  msg.guild.roles.find('name', discoR).setColor("DEFAULT");
-     }, 0)
-msg.channel.send(`Disco Durduruldu!`);
+  msg.guild.roles.find('name', discoR).setColor("RANDOM");
+   }, 8000);
+msg.channel.send(`Disco Başladı!`)
 };
 
 exports.conf = {
     enabled: true,
     guildOnly: true,
-    aliases: ['disko-durdur'],
+    aliases: ['disko'],
   kategori: 'ayarlar',
     permLevel: 3
 }
 
 exports.help = {
-    name: 'disco-durdur',
-    description: 'Disco Durdurur!',
-    usage: 'disco-durdur'
+    name: 'disco',
+    description: 'Disco',
+    usage: 'disco'
 }
