b64f349c5fef53606bca804755beab704b758897
Glitch (sunucubotu) <none>
2019-02-03 18:48:18 +0000

    🚐🎾 Checkpoint ./komutlar/js.js:2026984/81 ./komutlar/ping.js:2026984/87

--- komutlar/js.js
+++ komutlar/js.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 
 exports.run = function(client, message) {
-  var role = message.guild.roles.find(role => role.name === "JavaScript");
+  var role = message.guild.roles.find(role => role.name === "ROL"); // verilecek rol ismi
   message.member.addRole(role);
-  message.channel.send(`:white_check_mark: JavaScript rolü başarıyla verildi.`);
+  message.channel.send(`:white_check_mark: Rol ismi rolü başarıyla verildi.`);
 };
 
 exports.conf = {
