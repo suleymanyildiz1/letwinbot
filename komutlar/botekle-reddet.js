0627efba26c7d0159c7b7bda283eba1651dbdbeb
Glitch (sunucubotu) <none>
2019-02-13 15:16:14 +0000

    🏀🐕 Checkpoint ./komutlar/botekle-reddet.js:2616684/2

--- komutlar/botekle-reddet.js
+++ komutlar/botekle-reddet.js
@@ -4,8 +4,8 @@ const Discord = require('discord.js');
 exports.run = function(client, message, args) {
   
   if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`:no_entry: Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
-	let botisim = args[]
-  let sebep = args[2]
+	let botisim = args[0]
+  let sebep = args[1]
 	let log = "540617786880163902" // bot eklendi / onaylandı / reddedildi kanalı
 	
 	if (!botisim) return message.channel.send(`:no_entry: Botun idsini yazmalısın.`).then(msg => msg.delete(10000))
