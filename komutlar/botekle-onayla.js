66618ea23234740469df0c3203292361eaaf1a46
Glitch (sunucubotu) <none>
2019-02-13 16:44:28 +0000

    🏊🏼💄 Checkpoint ./komutlar/botekle-reddet.js:2616684/35 ./komutlar/botekle-onayla.js:2616684/71

--- komutlar/botekle-onayla.js
+++ komutlar/botekle-onayla.js
@@ -4,12 +4,13 @@ const Discord = require('discord.js');
 exports.run = function(client, message, args) {
   
   if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`:no_entry: Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
-	let botisim = args.join(" ")
+	let botisim = args[0]
+  let sahip = args[1]
 	let log = "540617786880163902" // bot eklendi / onaylandı / reddedildi kanalı
 	
 	if (!botisim) return message.channel.send(`:no_entry: Botun idsini yazmalısın.`).then(msg => msg.delete(10000))
   message.delete()
-		client.channels.get(log).send(`:tada:<@${botisim}> adlı bot onaylandı. Onaylayan yetkili : ${message.author}`);
+		client.channels.get(log).send(`:tada:<@${sahip}> adlı kişinin <@${botisim}> adlı botu onaylandı. Onaylayan yetkili : ${message.author}`);
 		message.channel.send(`:white_check_mark: Botu onayladınız.`).then(msg => msg.delete(10000))
 };
 
--- komutlar/botekle-reddet.js
+++ komutlar/botekle-reddet.js
@@ -6,13 +6,13 @@ exports.run = function(client, message, args) {
   if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`:no_entry: Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
 	let botisim = args[0]
   let sebep = args[2]
-  let sabip = args[1]
+  let sahip = args[1]
 	let log = "540617786880163902" // bot eklendi / onaylandı / reddedildi kanalı
 	
 	if (!botisim) return message.channel.send(`:no_entry: Botun idsini yazmalısın.`).then(msg => msg.delete(10000))
   if (!sebep) return message.channel.send(`:no_entry: Botu neden onaylamadığını yazmalısın.`).then(msg => msg.delete(10000))
   message.delete()
-		client.channels.get(log).send(`:no_entry: <@${botisim}> adlı bot reddedildi. Sebep : ${sebep} Reddeden yetkili : ${message.author}`);
+		client.channels.get(log).send(`:no_entry: <@${sahip}> adlı kişini <@${botisim}> adlı botu reddedildi. Sebep : ${sebep} Reddeden yetkili : ${message.author}`);
 		message.channel.send(`:white_check_mark: Botu reddettiniz.`).then(msg => msg.delete(10000))
 };
 
