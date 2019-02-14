b8baa61cfbc63d2161466912d1bb6af190d34983
Glitch (sunucubotu) <none>
2019-02-14 16:59:08 +0000

    🍳🌛 Checkpoint ./komutlar/botkapalı.js:2616684/2062

---
+++ komutlar/botkapalı.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message, args) {
+  
+  if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`:no_entry: Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
+	let botisim = args[0]
+  let sahip = args[1]
+  let sebep = args[2]
+  let sebep1 = args[3]
+  let sebep2 = args[4]
+  let sebep3 = args[5]
+  let sebep4 = args[6]
+  let sebep5 = args[7]
+  let sebep6 = args[8]
+  let sebep7 = args[9]
+  let sebep8 = args[10]
+  let sebep9 = args[11]
+	let log = "540617786880163902" // bot eklendi / onaylandı / reddedildi kanalı
+	
+	if (!botisim) return message.channel.send(`:no_entry: Botun idsini yazmalısın.`).then(msg => msg.delete(10000))
+    if (!sahip) return message.channel.send(`:no_entry: Bot Sahibi id yazman lazım.`).then(msg => msg.delete(10000))
+  message.delete()
+		client.channels.get(log).send(`:x: <@${botisim}> adlı bot kapalıdır. 3 günden fazla kapalı olursa atılacakdır! <@${sahip}> hemen halletmesi rica olunur`);
+		message.channel.send(`:white_check_mark: Botu reddettiniz.`).then(msg => msg.delete(10000))
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['bot-reddet', 'reddet'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'botkapalı', 
+  description: "Sunucuya eklenen botu reddeder.",
+  usage: 'botreddet <bot ismi> - <sebep>'
+};
