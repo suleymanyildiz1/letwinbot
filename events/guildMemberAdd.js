eedee4e733f158dd9e29d12938ce6f278edbabfc
Glitch (sunucubotu) <none>
2019-02-23 18:47:12 +0000

    🌷💨 Checkpoint ./events/guildMemberAdd.js:2616684/275

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,4 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.send('hoşgeldin.\nbot ekletmek istersen a!botekle botid prefix dblonaylı/onaysız yazman yeterli\nkodları görmek için a!js yazman yeterli!');
+};
