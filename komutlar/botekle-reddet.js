cf711c816bc7825e716bba72e59419cc162f1725
Glitch (sunucubotu) <none>
2020-09-24 10:56:13 +0000

    🚊🚝 Checkpoint ./komutlar/botekle-onayla.js:22846158/171 ./komutlar/botekle-reddet.js:22846158/480

--- komutlar/botekle-reddet.js
+++ komutlar/botekle-reddet.js
@@ -5,12 +5,12 @@ exports.run = function(client, message, args) {
   
   if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`:no_entry: Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
   
-	let lrowsisim = args[1]
-  let sahip = args[0]
+let botisim = message.guild.members.get(args[1])
+  let sahip = message.guild.members.get(args[0])
   let sebep = args[2]
   let sebep1 = args[3] 
   let sebep2 = args[4]
-  let sebep3 = args[5]
+  let sebep3 = args[5] 
   let sebep4 = args[6]
   let sebep5 = args[7]
   let sebep6 = args[8]
@@ -19,12 +19,13 @@ exports.run = function(client, message, args) {
   let sebep9 = args[11]
 	let log = "758379780751491143" // bot eklendi / onaylandı / reddedildi kanalı
 	
-	if (!lrowsisim) return message.channel.send(`:no_entry: Botun idsini yazmalısın.`).then(msg => msg.delete(10000))
+	if (!botisim) return message.channel.send(`:no_entry: Botun idsini yazmalısın.`).then(msg => msg.delete(10000))
   if (!sebep) return message.channel.send(`:no_entry: Botu neden onaylamadığını yazmalısın.`).then(msg => msg.delete(10000))
     if (!sahip) return message.channel.send(`:no_entry: Bot Sahibi id yazman lazım.`).then(msg => msg.delete(10000))
   message.delete()
-		client.channels.get(log).send(`<a:no:740278046921195612> <@${sahip}> _adlı kişinin_ <@${lrowsisim}> _adlı botu reddedildi. Sebep :_ **${sebep}** `);
+		client.channels.get(log).send(`<a:no:740278046921195612> ${sahip} _adlı kişinin_ ${botisim} _adlı botu reddedildi. | Sebep :_ **${sebep}** `);
 		message.channel.send(`<a:tik4:756946179530424541> Botu reddettiniz.`).then(msg => msg.delete(10000))
+    sahip.send(`<a:no:740278046921195612>  ${botisim} _**Adlı botunuz Jau Bot List'te reddedildi. | Sebep :**_ **${sebep}**`)
 };
 
 exports.conf = {
