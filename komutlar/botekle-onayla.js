6fd9b315ffae9a93dfb4d4a150b89f99a31187b2
Glitch (sunucubotu) <none>
2020-09-24 10:44:21 +0000

    🌛👢 Checkpoint ./komutlar/botekle-onayla.js:22846158/439 ./komutlar/botekle-reddet.js:22846158/143

--- komutlar/botekle-onayla.js
+++ komutlar/botekle-onayla.js
@@ -5,16 +5,19 @@ exports.run = function(client, message, args) {
   
   if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`:no_entry: Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
   
-	let botisim = args[0]
-  let sahip = args[1]
+	let botisim = message.guild.members.get(args[1])
+  let sahip = message.guild.members.get(args[0])
 	let log = "758379780751491143" // bot eklendi / onaylandı / reddedildi kanalı
 	let rol = '758382847005949962'
+  let b = '734904078114750526'
+
 	if (!botisim) return message.channel.send(`:no_entry: Botun idsini yazmalısın.`).then(msg => msg.delete(10000))
   if(!sahip) return message.reply("Bir sahip id girmen gerekiyor")
   message.delete()
     sahip.addRole(rol)
+  botisim.addRole(b)
    
-		client.channels.get(log).send(`<a:tik4:756946179530424541> <@${sahip}> adlı kişinin <@${botisim}> adlı botu onaylandı.`)
+		client.channels.get(log).send(`<a:tik4:756946179530424541> ${sahip} adlı kişinin ${botisim} adlı botu onaylandı.`)
 		message.channel.send(`<a:tik4:756946179530424541> Botu onayladınız.`).then(msg => msg.delete(2000))
     
   
--- komutlar/botekle-reddet.js
+++ komutlar/botekle-reddet.js
@@ -4,10 +4,11 @@ const Discord = require('discord.js');
 exports.run = function(client, message, args) {
   
   if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`:no_entry: Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
-	let lrowsisim = args[0]
-  let sahip = args[1]
+  
+	let lrowsisim = args[1]
+  let sahip = args[0]
   let sebep = args[2]
-  let sebep1 = args[3]
+  let sebep1 = args[3] 
   let sebep2 = args[4]
   let sebep3 = args[5]
   let sebep4 = args[6]
@@ -22,7 +23,7 @@ exports.run = function(client, message, args) {
   if (!sebep) return message.channel.send(`:no_entry: Botu neden onaylamadığını yazmalısın.`).then(msg => msg.delete(10000))
     if (!sahip) return message.channel.send(`:no_entry: Bot Sahibi id yazman lazım.`).then(msg => msg.delete(10000))
   message.delete()
-		client.channels.get(log).send(`<a:no:740278046921195612> <@${sahip}> _adlı kişinin_ <@${lrowsisim}> _adlı botu reddedildi. Sebep :_ **${sebep} ${sebep1} ** `);
+		client.channels.get(log).send(`<a:no:740278046921195612> <@${sahip}> _adlı kişinin_ <@${lrowsisim}> _adlı botu reddedildi. Sebep :_ **${sebep}** `);
 		message.channel.send(`<a:tik4:756946179530424541> Botu reddettiniz.`).then(msg => msg.delete(10000))
 };
 
