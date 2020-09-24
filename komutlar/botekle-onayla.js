d2d91b45d19b20a86148a95ab658dd54897a2aa4
Glitch (sunucubotu) <none>
2020-09-24 10:33:18 +0000

    🗺💫 Checkpoint ./komutlar/botekle-onayla.js:22846158/228

--- komutlar/botekle-onayla.js
+++ komutlar/botekle-onayla.js
@@ -5,16 +5,16 @@ exports.run = function(client, message, args) {
   
   if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`:no_entry: Bu komutu kullanabilmek için "\`Yönetici\`" yetkisine sahip olmalısın.`);
   
-	let lrowsisim = args[0]
+	let botisim = args[0]
   let sahip = args[1]
 	let log = "758379780751491143" // bot eklendi / onaylandı / reddedildi kanalı
-	let rol = '736326897327145052'
-	if (!lrowsisim) return message.channel.send(`:no_entry: Botun idsini yazmalısın.`).then(msg => msg.delete(10000))
+	let rol = '758382847005949962'
+	if (!botisim) return message.channel.send(`:no_entry: Botun idsini yazmalısın.`).then(msg => msg.delete(10000))
   if(!sahip) return message.reply("Bir sahip id girmen gerekiyor")
   message.delete()
     sahip.addRole(rol)
    
-		client.channels.get(log).send(`<a:tik4:756946179530424541> <@${sahip}> adlı kişinin <@${lrowsisim}> adlı botu onaylandı.`)
+		client.channels.get(log).send(`<a:tik4:756946179530424541> <@${sahip}> adlı kişinin <@${botisim}> adlı botu onaylandı.`)
 		message.channel.send(`<a:tik4:756946179530424541> Botu onayladınız.`).then(msg => msg.delete(2000))
     
   
