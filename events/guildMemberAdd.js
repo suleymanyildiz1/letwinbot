59c2701bd5bfe1a67567ffdddbfce6f97161528c
Glitch (sunucubotu) <none>
2020-09-30 14:24:58 +0000

    👾👴 Checkpoint ./events/guildMemberAdd.js:22846158/107

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,4 @@
 module.exports = member => {
     let username = member.user.username;
-    member.send('hoşgeldin.\nbot ekletmek istersen j!botekle botid prefix dblonaylı/onaysız yazman yeterli');
+  
 };
