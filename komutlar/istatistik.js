a9ab01225e97f5c00d949ba78540c74efe2f45d4
Glitch (confirmed-motley-mojoceratops) <none>
2021-02-10 07:03:33 +0000

    🍵⛱ Checkpoint ./komutlar/istatistik.js:23258541/32

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -7,16 +7,16 @@ const Aktiflik = moment.duration(client.uptime).format(' D [Gün], H [Saat], m [
 const Letwin = new Discord.MessageEmbed()
 .setAuthor(client.user.username+' - İstatistik',client.user.avatarURL())
 .addField('**Genel Veriler**',`
-Sunucu » \`${client.guilds.cache.size}\`
-Kullanıcı » \`${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}\`
-Gecikme » \`${client.ws.ping}\`
-Bellek » \`${(process.memoryUsage().heapUsed / 2048 / 2048).toFixed(2)}\`
-Aktiflik » \`${Aktiflik}\``)
+Sunucu **»** \`${client.guilds.cache.size}\`
+Kullanıcı **»** \`${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}\`
+Gecikme **»** \`${client.ws.ping}\`
+Bellek **»** \`${(process.memoryUsage().heapUsed / 2048 / 2048).toFixed(2)}\`
+Aktiflik **»** \`${Aktiflik}\``)
 .addField('**Genel Bilgiler**',`
-BOT Sahibi » \`${client.users.cache.get(ayarlar.sahip).tag}\``)
+BOT Sahibi **»** \`${client.users.cache.get(ayarlar.sahip).tag}\``)
 .addField('**Sürümler**',`
-Discord.JS » \`${Discord.version}\`
-Node.JS » \`${process.version}\``)
+Discord.JS **»** \`${Discord.version}\`
+Node.JS **»** \`${process.version}\``)
 .setThumbnail(client.user.avatarURL())
 .setFooter(`© ${client.user.username}`,client.user.avatarURL())
 message.channel.send(Letwin)
